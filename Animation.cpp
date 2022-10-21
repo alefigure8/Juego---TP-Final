d963edfe8dd8318100ea9e49b736200a293fce95
agome <agome@DESKTOP-81ER52L>
2022-10-21 10:09:25 -0300

    Adding methods and properties to Animation

--- Animation.cpp
+++ Animation.cpp
@@ -1,1 +1,32 @@
 #include "Animation.h"
+Animation::Animation(std::string image, sf::Vector2u imageCount)
+{
+	_texture.loadFromFile(image);
+	this->imageCount = imageCount;
+	currentImage.x = 0;
+
+	uvRect.width = _texture.getSize().x / float(imageCount.x);
+	uvRect.height = _texture.getSize().y / float(imageCount.y);
+}
+
+Animation::~Animation()
+{
+}
+
+void Animation::update()
+{
+	if (currentImage.x >= imageCount.x)
+		currentImage.x = imageCount.x - 1;
+
+	uvRect.left = currentImage.x * uvRect.width;
+	uvRect.top = currentImage.y * uvRect.height;
+}
+sf::Vector2u Animation::getCurrentImage()
+{
+	return currentImage;
+}
+
+void Animation::setCurrentImage(sf::Vector2u imageCurrent)
+{
+	currentImage = imageCurrent;
+}
--- Animation.h
+++ Animation.h
@@ -1,5 +1,18 @@
 #pragma once
+#include <SFML/Graphics.hpp>
 class Animation
 {
+public:
+	sf::IntRect uvRect;
+	sf::Texture _texture;
+	Animation(std::string image, sf::Vector2u imageCount);
+	~Animation();
+	void update();
+	sf::Vector2u getCurrentImage();
+	void setCurrentImage(sf::Vector2u imageCurrent);
+
+private:
+	sf::Vector2u imageCount;
+	sf::Vector2u currentImage;
 };
 
