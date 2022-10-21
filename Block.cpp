c03c699144e3b1ed35c19c306554b8cbeaeb2ab5
agome <agome@DESKTOP-81ER52L>
2022-10-21 12:08:36 -0300

    Adding methods and properties

--- Block.cpp
+++ Block.cpp
@@ -1,1 +1,54 @@
 #include "Block.h"
+
+void Block::_initTexture(std::string image, sf::Vector2u imageCount)
+{
+	//Load Texture
+	if (!_texture.loadFromFile(image))
+		std::cout << "ERROR::PLAYER::INITTEXTURE::Could not load texture." << std::endl;
+}
+
+Block::Block(std::string image, sf::Vector2u imageCount) : _animation(image, imageCount)
+{
+	_initTexture(image, imageCount);
+	_initSprite();
+	_life = imageCount.x;
+	_max_life = imageCount.x;
+}
+
+Block::~Block()
+{
+
+}
+
+int Block::getLife()
+{
+	return _life;
+}
+
+void Block::setLife(int life)
+{
+	_life = life;
+}
+
+void Block::setAngle(float deg)
+{
+	_sprite.rotate(deg);
+}
+
+void Block::update()
+{
+	// Update animacion con la vida que le queda al objeto
+	if (_life <= 0)
+		_life = 0;
+	sf::Vector2u currentImage = _animation.getCurrentImage();
+	currentImage.x = _max_life - _life;
+	_animation.setCurrentImage(currentImage);
+	_sprite.setTextureRect(_animation.uvRect);
+	_sprite.setOrigin(_animation.uvRect.width / 2, _animation.uvRect.height / 2);
+	_animation.update();
+}
+
+void Block::setScale(sf::Vector2f scale)
+{
+	_sprite.setScale(scale);
+}
--- Block.h
+++ Block.h
@@ -1,5 +1,24 @@
 #pragma once
-class Block
+#include <iostream>
+#include "SFML/Graphics.hpp"
+#include "Base.h"
+#include "Animation.h"
+
+class Block 
+	: protected Base
 {
+private:
+	Animation _animation;
+	int _life;
+	int _max_life;
+	void _initTexture(std::string image, sf::Vector2u imageCount);
+public:
+	Block(std::string image, sf::Vector2u imageCount);
+	~Block();
+	int getLife();
+	void setLife(int life);
+	void setScale(sf::Vector2f scale);
+	void setAngle(float deg);
+	void update();
 };
 
