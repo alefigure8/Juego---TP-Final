2fe1b06fa252e25e5b63aec282392c0077921bf2
agome <agome@DESKTOP-81ER52L>
2022-10-22 15:44:09 -0300

    Adding method and properties

--- Armor.cpp
+++ Armor.cpp
@@ -1,1 +1,26 @@
 #include "Armor.h"
+
+sf::Sprite Armor::getArmor()
+{
+	return _sprite;
+}
+
+float Armor::getRotationArmor()
+{
+	return _sprite.getRotation();
+}
+
+sf::Vector2f Armor::getOriginArmor()
+{
+	return _sprite.getOrigin();
+}
+
+void Armor::setRotation(float degree)
+{
+	_sprite.setRotation(degree);
+}
+
+void Armor::move(float x, float y)
+{
+	_sprite.rotate(x);
+}
--- Armor.h
+++ Armor.h
@@ -1,5 +1,16 @@
 #pragma once
+#include <iostream>
+#include "SFML/Graphics.hpp"
+#include "Base.h"
+
 class Armor
+	: protected Base
 {
+public:
+	sf::Sprite getArmor();
+	float getRotationArmor();
+	sf::Vector2f getOriginArmor();
+	void setRotation(float degree);
+	void move(float x, float y);
 };
 
