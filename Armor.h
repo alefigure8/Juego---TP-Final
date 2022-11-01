f746f765751c7bbd92316e57e678f410365fe021
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2022-11-01 10:28:03 -0300

    adding animation to armor

--- Armor.h
+++ Armor.h
@@ -2,16 +2,27 @@
 #include <iostream>
 #include "SFML/Graphics.hpp"
 #include "Base.h"
+#include "Animation.h"
+#include "Effect.h"
 
 class Armor
 	: public Base
 {
+protected:
+	Animation _animation;
 public:
-	Armor(std::string image);
+	Armor(std::string image, sf::Vector2u imageCount);
+
+	//getter
 	sf::Sprite getArmor();
 	float getRotationArmor();
 	sf::Vector2f getOriginArmor();
+	
+	//setter
 	void setRotation(float degree);
+
+	//methods
 	void move(float x, float y);
+	void updateAnimation(sf::Vector2u currentImagect);
 };
 
